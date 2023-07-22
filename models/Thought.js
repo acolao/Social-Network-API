472c688d737f0389309b39aecae8de6d7684f3e0
Alison Colao <alison.siwek@gmail.com>
2023-07-22 15:14:10 -0700

    added thought model

---
+++ models/Thought.js
@@ -0,0 +1,62 @@
+const { Schema, Types, model } = require('mongoose');
+
+const reactionSchema = new mongoose.Schema(
+    {
+    reactionId: {
+        type: Types.ObjectId,
+        default: new Types.ObjectId(),
+    },
+    reactionBody: {
+        type: string,
+        required: true,
+        maxLength: 280,
+    },
+    username: {
+        type: string,
+        required: true,
+    },
+    createdAt: {
+        type: Date,
+        default: () => Date.now(),
+    },
+    },
+    {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+    }
+  );
+
+  const thoughSchema = new Schema(
+    {
+       thoughtText: {
+        type: string,
+        required: true,
+        minlength: 1,
+        maxLength: 280,
+       },
+       createdAt: {
+        type: Date,
+        default: () => Date.now(),
+       },
+       username: {
+        type: string,
+        required: true,
+       },
+       reactiions: [reactionSchema],
+    },
+  );
+       {
+        toJSON: {
+            virtuals: true,    
+        },
+        id: false,
+        }
+        thoughtSchema.virtuals("reactionCount").get(function () {
+            return this.reactions.length;
+        });
+
+        const Thought = model("Thought", thoughtSchema);
+
+        module.exports = { Thought, thoughtSchema };
